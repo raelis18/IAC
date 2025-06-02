f3d8c573c02065735e19f690b0f39034c2cc6ad9
Israel Santos <59796508+raelis18@users.noreply.github.com>
2025-06-02 19:22:31 -0300

    add:datasources

--- modules/s3/outputs.tf
+++ modules/s3/outputs.tf
@@ -1,5 +1,5 @@
 output "bucket_domain_name" {
-  value       = data.aws_s3_bucket.bucket.bucket
+  value       = data.aws_s3_bucket.bucket.bucket_domain_name
   sensitive   = false
   description = "Domain name of the S3 bucket"
 }
